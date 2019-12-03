019acdeca1f7cf93c3437fcba89ce34f11740079
Rômulo Guimarães <romulo1984@gmail.com>
2019-12-03 11:30:04 -0300

    Changed match item style

--- src/components/MatchItem.vue
+++ src/components/MatchItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="match-item">
+  <div class="match-item shadow">
     <div class="row">
       <div class="col-12 text-center small">
         <div class="match-item-datetime">
@@ -9,7 +9,7 @@
       </div>
     </div>
 
-    <div class="row d-flex justify-content-center pb-3 pt-4 mt-sm-4">
+    <div class="row d-flex justify-content-center pb-3 mt-3">
       <div class="col-sm-4 text-right mx-3 mx-sm-0">
         <TeamItem :team="match.fields.host" badge-side="right"/>
       </div>
@@ -51,21 +51,12 @@ export default {
     font-family: 'Roboto', sans-serif;
   }
   .match-item {
-    border: 1px solid #ccc;
+    border: 1px solid rgba(0, 0, 0, .12);
     border-radius: 10px;
     margin-bottom: 15px;
     padding-top: 10px;
   }
   .match-item-datetime {
-    position: absolute;
-    margin: 0 auto;
-    top: -5;
-    left: 0;
-    right: 0;
-    padding: 6px;
-    border: 1px solid #ccc;
-    border-radius: 6px;
-    background-color: #fff;
     @media (max-width: 575px) {
       position: relative;
       border: none;
